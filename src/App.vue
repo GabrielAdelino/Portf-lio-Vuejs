fa7228958c1127a68098f295b90459b832e26d09
Gabriel Adelino <gabriel.adelinodev@gmail.com>
2024-10-25 15:46:08 -0300

    firts commit

---
+++ src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="app">
+    <Header/>
+      <section id="home">
+        <h2>Seção1</h2>
+        <p>Conteúdo da seção 1.</p>
+      </section>
+
+      <section id="about">
+        <h2>Seção1</h2>
+        <p>Conteúdo da seção 1.</p>
+      </section>
+
+      <section id="projects">
+        <h2>Seção1</h2>
+        <p>Conteúdo da seção 1.</p>
+      </section>
+
+      <section id="contact">
+        <h2>Seção1</h2>
+        <p>Conteúdo da seção 1.</p>
+      </section>
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+  },
+}
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+html {
+  scroll-behavior: smooth;
+}
+/* section {
+  margin-top: 500px;    Teste para saber se direcionamento funciona
+} */
+
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  /* margin-top: 60px; */
+}
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+    <header>
+      <nav>
+        <ul>
+          <li v-for="(link, index) in navLinks" :key="index">
+            <a :href="link.href">{{ link.text }}</a>
+            </li>
+        </ul>
+      </nav>
+    </header>
+  </template>
+  
+  <script>
+  export default {
+    name: 'AppHeader',
+    data() {
+      return {
+        navLinks: [
+          { text: 'Home', href: '#home' },
+          { text: 'About', href: '#about' },
+          { text: 'Projects', href: '#projects' },
+          { text: 'Contact', href: '#contact' }
+        ],
+      };
+  },
+  };
+  </script>
+  
+  <style scoped>
+    header {
+  background-color: #333;
+  color: white;
+  margin-top: 0;
+  padding: 20px;
+}
+
+nav ul {
+  list-style-type: none;
+  display: flex;
+  justify-content: space-between;
+  padding: 0;
+}
+
+nav ul li a {
+  color: white;
+  text-decoration: none;
+  font-weight: bold;
+}
+
+nav ul li a:hover {
+  text-decoration: underline;
+}
+  </style>
+  

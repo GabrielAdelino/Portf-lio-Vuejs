7232c6433dc485d2a9c9c77f9cd34349a62c5285
Gabriel Adelino <gabriel.adelinodev@gmail.com>
2024-10-27 11:48:56 -0300

    Second commit

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -3,36 +3,41 @@
       <nav>
         <ul>
           <li v-for="(link, index) in navLinks" :key="index">
-            <a :href="link.href">{{ link.text }}</a>
+            <a :href="link.href">
+              <span :class="link.iconStyle">{{ link.icon }}</span>
+              {{ link.text }}</a>
             </li>
         </ul>
       </nav>
     </header>
   </template>
-  
+
   <script>
   export default {
     name: 'AppHeader',
     data() {
       return {
         navLinks: [
-          { text: 'Home', href: '#home' },
-          { text: 'About', href: '#about' },
-          { text: 'Projects', href: '#projects' },
-          { text: 'Contact', href: '#contact' }
+          { text: 'Home', href: '#home', icon: "home", iconStyle: "material-icons-outlined"},
+          { text: 'About', href: '#about', icon: "account_circle", iconStyle: "material-icons-outlined"},
+          { text: 'Projects', href: '#projects', icon: "devices", iconStyle: "material-icons-outlined"},
+          { text: 'Contact', href: '#contact', icon: "mail", iconStyle: "material-icons-outlined" }
         ],
       };
   },
   };
   </script>
-  
+
   <style scoped>
+
     header {
   background-color: #333;
   color: white;
   margin-top: 0;
   padding: 20px;
 }
+
+
 
 nav ul {
   list-style-type: none;
@@ -44,11 +49,24 @@ nav ul {
 nav ul li a {
   color: white;
   text-decoration: none;
+  display: inline-block;
+  margin-left: 50px;
+  font-size: 20px;
+  margin-right: 50px; 
   font-weight: bold;
+  align-items: center;
+  display: flex;
 }
 
-nav ul li a:hover {
-  text-decoration: underline;
+.material-icons-outlined{
+  display: inline-block;
+  text-decoration: none;
+  font-size: 30px;
+  margin-right: 5px;
+  margin-bottom: 5px;
 }
+
+/* nav ul li a:hover {
+  text-decoration: underline;
+} */
   </style>
-  
